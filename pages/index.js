dfacdaa56af78ed090b0083a296bb6bf01c85946
user-nicoco <macbook.nicolas.barros@gmail.com>
2024-05-13 20:31:03 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="1 er site" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          va voir la <code>pages/index.js</code>
         </p>
       </main>
 
