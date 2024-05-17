4ba4184b9a10aaad87c941f9b23a9fd0f607dced
user-nicoco <macbook.nicolas.barros@gmail.com>
2024-05-17 09:13:42 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,6 +9,7 @@ const Header = ({ title }) => (
           <li><a href="#about">À propos</a></li>
           <li><a href="#skills">Compétences</a></li>
           <li><a href="#projects">Projets</a></li>
+          <li><a href="#testimonials">Témoignages</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
