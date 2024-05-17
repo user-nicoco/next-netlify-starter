aefcd03bdfad1d082b564048489c32a3ec852fac
user-nicoco <macbook.nicolas.barros@gmail.com>
2024-05-17 08:56:55 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,19 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
-}
+// components/Header.js
+const Header = ({ title }) => (
+  <header>
+    <div className="container">
+      <h1>{title}</h1>
+      <p>Ingénieur Système</p>
+      <nav>
+        <ul>
+          <li><a href="#about">À propos</a></li>
+          <li><a href="#skills">Compétences</a></li>
+          <li><a href="#projects">Projets</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+);
+
+export default Header;
