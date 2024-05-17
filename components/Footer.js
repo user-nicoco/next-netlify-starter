dbf4f50005b989b61765b781529d310235ef2298
user-nicoco <macbook.nicolas.barros@gmail.com>
2024-05-17 09:28:10 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,13 @@
-// components/Footer.js
-const Footer = () => (
-  <footer>
-    <div className="container">
-      <p>&copy; 2024 Nicolas Barros. Tous droits réservés.</p>
-    </div>
-  </footer>
-);
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-2">
+      <div className="container mx-auto px-4">
+        <p>&copy; 2024 Nicolas Barros. Tous droits réservés.</p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
