cc94a5b2326f512d016d7121dc1c4f9b68e953ea
user-nicoco <macbook.nicolas.barros@gmail.com>
2024-05-17 09:09:07 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
-import '@styles/globals.css'
+
+
+// pages/_app.js
+import '../styles/globals.css'
+import 'aos/dist/aos.css'
+import AOS from 'aos'
+import { useEffect } from 'react'
 
 function Application({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    })
+  }, [])
+
   return <Component {...pageProps} />
 }
 
