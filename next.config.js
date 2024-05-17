36c89b71ad3cc328cf598217a2c00cb653d891b1
user-nicoco <macbook.nicolas.barros@gmail.com>
2024-05-17 11:32:19 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  trailingSlash: true, // Ajouter des barres obliques à la fin des URL
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      '/': { page: '/' },
+      // Ajoutez d'autres pages ici si nécessaire
+    };
+  }
 };
 
 module.exports = nextConfig;
